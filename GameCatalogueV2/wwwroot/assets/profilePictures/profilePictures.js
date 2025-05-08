61c3695666e5eb01993161a2a453f1512a80197f
Yourixf <Yourivanbeuzekom@upcmail.nl>
2025-05-09 01:11:49 +0200

    base project

---
+++ GameCatalogueV2/wwwroot/assets/profilePictures/profilePictures.js
@@ -0,0 +1,31 @@
+import defaultProfile from "./defaultProfile.png";
+import boy1 from "./boy1.png";
+import boy2 from "./boy2.png";
+import cat1 from "./cat1.png";
+import cat2 from "./cat2.png";
+import devil from "./devil.png";
+import dog1 from "./dog1.png";
+import dog2 from "./dog2.png";
+import evilGood from "./evilGood.png";
+import frankenstein from "./frankenstein.png";
+import man1 from "./man1.png";
+import man2 from "./man2.png";
+import woman1 from "./woman1.png";
+import woman2 from "./woman2.png";
+
+export const profilePictures = [
+    { label: "defaultProfile", file: "defaultProfile.png", src: defaultProfile },
+    { label: "boy1", file: "boy1.png", src: boy1 },
+    { label: "boy2", file: "boy2.png", src: boy2 },
+    { label: "cat1", file: "cat1.png", src: cat1 },
+    { label: "cat2", file: "cat2.png", src: cat2 },
+    { label: "devil", file: "devil.png", src: devil },
+    { label: "dog1", file: "dog1.png", src: dog1 },
+    { label: "dog2", file: "dog2.png", src: dog2 },
+    { label: "frankenstein", file: "frankenstein.png", src: frankenstein },
+    { label: "evilGood", file: "evilGood.png", src: evilGood },
+    { label: "man1", file: "man1.png", src: man1 },
+    { label: "man2", file: "man2.png", src: man2 },
+    { label: "woman1", file: "woman1.png", src: woman1 },
+    { label: "woman2", file: "woman2.png", src: woman2 },
+];
